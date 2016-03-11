b8f555d8bca8b9ade432a0795653a9fb271d11e6
MICHAEL J MELTON <meltonm83@gmail.com>
2016-03-11 13:16:23 -0800

    to merge

---
+++ apps/app.js
@@ -0,0 +1,35 @@
+$(function(){
+	
+	//$(function(){
+	  $('#search-term').submit(function(event){
+	    event.preventDefault();
+	    var searchTerm = $('#query').val();
+	    getRequest(searchTerm);
+	  });
+	//});
+	
+    function getRequest(searchTerm){
+		var params = {
+			part: 'snippet',
+			key: "AIzaSyBa-ZI_KNK9WXHksjtxQoboYO_-GHwGRW4",
+			q: searchTerm
+		};
+		url = 'https://www.googleapis.com/youtube/v3/search';
+		
+		$.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&q=abba&key=AIzaSyDRR0OY18AImRUUhQi5kuHBMGJ7EO7C2_w', function(data){
+			showResults(data);
+		});
+	};
+	
+	function showResults(results) {
+		var html = '<p>Results:</p><br>';
+		$.each(results.items, function(index,value){
+			html += '<p>' + value.snippet.title + '</p><br>' +
+			value.snippet.thumbnails.medium +
+			'</p><br>';
+			console.log(value.snippet.title, value.snippet.thumbnails.medium);	
+		});
+		$('#search-results').html(html);
+		};
+	
+});
